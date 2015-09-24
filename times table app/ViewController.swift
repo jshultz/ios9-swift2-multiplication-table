36d769ed730b7f0d481d9d359cc113ab4ed621f3
Jason Shultz <jasshultz@gmail.com>
2015-09-24 11:49:56 -0600

    init commit

--- times table app/ViewController.swift
+++ times table app/ViewController.swift
@@ -15,6 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        print(sliderValue)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
